6fd62891e09a3612e1165b16ee0c18a3a6f52cd3
ioriveur(iori-yja) <fivo.11235813@gmail.com>
2011-08-20 23:43:00 +0900

    openbsd.sh

--- songs/openbsd.sh
+++ songs/openbsd.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/bin/dash
 
-for x in $(seq $1 $2)
+if test -z $1
+then
+	export p=31
+	export q=49
+else
+	export p=$1
+	export q=$2
+fi
+
+for x in $(seq $p $q)
 do
 	notify-send OpenBSD:$x
 	mplayer	song$x.ogg
